bc28e7b98717a2e82f7f24d01b471351ba1d46f7
Lútsen Stellingwerff <lutsen.stellingwerff@gmail.com>
2017-05-17 11:23:44 +0200

    Check returns logged in user array.

--- controllers/Auth.php
+++ controllers/Auth.php
@@ -20,20 +20,22 @@ class Auth {
 	}
 
 	/**
-	 * Check if logged in user has right properties
+	 * Check if logged in user has right properties. Throws an exception if not.
 	 *
 	 * @var string[] $allowed String with single id or array with allowed user id's
 	 *
-	 * @return boolean Returns true if logged in user id is in $allowed, false otherwise
+	 * @return string[] Array with logged in user name, email and id.
 	 */
 	public function check( $allowed ) {
 		if ( isset( $this->session->authenticated ) ) {
 			if (
-				( is_array( $allowed ) && !in_array( $app->session->authenticated['id'], $allowed ) )
+				( is_array( $allowed ) && in_array( $app->session->authenticated['id'], $allowed ) )
 				||
-				$this->session->authenticated['id'] != $allowed
+				$this->session->authenticated['id'] == $allowed
 
 			) {
+				return $this->session->authenticated;
+			} else {
 				throw new \Exception('You are not allowed to perform this action.');
 			}
 		} else {
